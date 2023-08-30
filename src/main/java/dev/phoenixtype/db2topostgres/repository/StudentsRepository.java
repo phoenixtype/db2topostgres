6e132db304f2b3c3d7af0e2558e63bead50bd984
phoenixtype <samuelakuma130@gmail.com>
2023-08-29 20:32:36 -0400

    update

--- src/main/java/dev/phoenixtype/db2topostgres/repository/StudentsRepository.java
+++ src/main/java/dev/phoenixtype/db2topostgres/repository/StudentsRepository.java
@@ -1,7 +1,6 @@
 package dev.phoenixtype.db2topostgres.repository;
 
 
-
 import dev.phoenixtype.db2topostgres.model.StudentRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
